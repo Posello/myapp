1ef0cf7fb3be9562cfd286c60385b87424bfdfba
Posello <xavierrosello@telefonica.net>
2023-10-21 10:52:46 +0200

    #1 Reloj

--- src/routes/reloj.ts
+++ src/routes/reloj.ts
@@ -1,1 +1,11 @@
-"Prueba"
+const clock: HTMLSpanElement = document.getElementById('clock');
+
+setInterval(()=>{
+    const date: Date = new Date();
+    clock.innerText = date.toLocaleDateString('es-ES',{
+        hour:'2-digit',
+        minute:'2-digit',
+        second:'2-digit'
+    });
+
+}, 1000)
