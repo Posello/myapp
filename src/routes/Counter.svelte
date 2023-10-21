953519818ac28c86e8b55410baf682cbcbff239e
HairyCorn <ariel.carrillo96@gmail.com>
2023-10-21 12:34:27 +0200

    #2 Cambios en la visualización de la variable cock

--- src/routes/Counter.svelte
+++ src/routes/Counter.svelte
@@ -5,7 +5,7 @@
 
 	const displayed_count = spring();
 	$: displayed_count.set(count);
-	$: offset = modulo($displayed_count, 1);
+	// $: offset = modulo($displayed_count, 1);
 
 	function modulo(n: number, m: number) {
 		// handle negative numbers
@@ -37,24 +37,23 @@
 <span>{reloj}</span>
 
 <div class="counter">
-	<button on:click={() => (count -= 1)} aria-label="Decrease the counter by one">
+	<!-- <button on:click={() => (count -= 1)} aria-label="Decrease the counter by one">
 		<svg aria-hidden="true" viewBox="0 0 1 1">
 			<path d="M0,0.5 L1,0.5" />
 		</svg>
-	</button>
+	</button> -->
 
 	<div class="counter-viewport">
-		<div class="counter-digits" style="transform: translate(0, {100 * offset}%)">
-			<strong class="hidden" aria-hidden="true">{Math.floor($displayed_count + 1)}</strong>
-			<strong>{Math.floor($displayed_count)}</strong>
+		<div class="counter-digits">
+			<strong>{reloj}</strong>
 		</div>
 	</div>
 
-	<button on:click={() => (count += 1)} aria-label="Increase the counter by one">
+	<!-- <button on:click={() => (count += 1)} aria-label="Increase the counter by one">
 		<svg aria-hidden="true" viewBox="0 0 1 1">
 			<path d="M0,0.5 L1,0.5 M0.5,0 L0.5,1" />
 		</svg>
-	</button>
+	</button> -->
 </div>
 
 <style>
@@ -63,6 +62,7 @@
 		border-top: 1px solid rgba(0, 0, 0, 0.1);
 		border-bottom: 1px solid rgba(0, 0, 0, 0.1);
 		margin: 1rem 0;
+		width: 100%;
 	}
 
 	.counter button {
@@ -93,7 +93,7 @@
 	}
 
 	.counter-viewport {
-		width: 8em;
+		width: 100%;
 		height: 4em;
 		overflow: hidden;
 		text-align: center;
